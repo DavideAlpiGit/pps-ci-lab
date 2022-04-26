08cf733e70154739965959b3b118d570daf54640
paolo <paolo.penazzi@studio.unibo.it>
2022-04-26 16:16:32 +0200

    Create build.gradle file

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,5 +1,9 @@
-public class Database() {
+public class Database {
   public Database() {
       System.out.println("Paolo");
   }
+
+  public static void main(String[] args) {
+        Database database = new Database();
+    }
 }
